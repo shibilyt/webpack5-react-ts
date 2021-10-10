919a512a86c2d77ac1c1d5598e78026ae03f0ce0
Shibily <shibilyt@gmail.com>
2021-10-10 08:01:44 +0530

    Add babel loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,6 +3,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "..", "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
