99fcbb5ca854562fcea2e7c949756edea26dabcb
Shibily <shibilyt@gmail.com>
2021-10-14 22:29:00 +0530

    Add react-refresh support for dev

--- config/webpack.dev.ts
+++ config/webpack.dev.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 const config: Configuration = {
   mode: "development",
   devtool: "cheap-module-source-map",
-  plugins: [new HotModuleReplacementPlugin()],
+  plugins: [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()],
   devServer: {
     static: {
       directory: path.resolve(__dirname, "..", "./build"),
